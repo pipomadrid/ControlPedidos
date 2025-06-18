6a6c95c6ba5317780a4a98d11202f848fd0db960
PedroJoseSaez <pedrojosesaezperez@gmail.com>
2025-06-18 11:27:02 +0200

    Cambios en POM

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,13 @@ module ControlPedidos {
     requires java.prefs;
     requires org.apache.poi.ooxml;
 
+    requires org.apache.poi.ooxml.schemas;
+    requires org.apache.poi.poi;
+    requires org.apache.commons.collections4;
+    requires org.apache.xmlbeans;
+    requires org.apache.commons.compress;
+
+
     opens com.pedrojosesaez.controlpedidos to javafx.fxml,org.apache.poi.ooxml,org.apache.poi.ooxml.schemas;
     exports com.pedrojosesaez.controlpedidos;
 }
