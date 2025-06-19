b73b349813f23a9ad95f9cd02be5b4e222f8cf7c
PedroJoseSaez <pedrojosesaezperez@gmail.com>
2025-06-19 08:30:57 +0200

    Se cambia el POM, versión funcionando

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,17 +2,16 @@ module ControlPedidos {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires javafx.base;
     requires java.desktop;
     requires java.prefs;
     requires org.apache.poi.ooxml;
-
-    requires org.apache.poi.ooxml.schemas;
     requires org.apache.poi.poi;
     requires org.apache.commons.collections4;
     requires org.apache.xmlbeans;
     requires org.apache.commons.compress;
 
 
-    opens com.pedrojosesaez.controlpedidos to javafx.fxml,org.apache.poi.ooxml,org.apache.poi.ooxml.schemas;
+    opens com.pedrojosesaez.controlpedidos to javafx.fxml;
     exports com.pedrojosesaez.controlpedidos;
 }
